62b43e4c6c65c5885023ef39da00c914447b1dba
George Trammell <gtram@bu.edu>
2024-03-17 17:04:51 -0400

    George Trammell: Assignment 6

---
+++ assignments/containerfiles/GEORGE-TRAMMELL/assignment6.py
@@ -0,0 +1,16 @@
+# george trammell
+import random
+
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "I'm reading a book on anti-gravity. It's impossible to put down!",
+    "Did you hear about the mathematician who’s afraid of negative numbers? He'll stop at nothing to avoid them.",
+    "Why was the math book sad? Because it had too many problems.",
+    "Parallel lines have so much in common. It’s a shame they’ll never meet.",
+    "Why do we tell actors to 'break a leg'? Because every play has a cast.",
+    "Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, “What’s the word on the street?”",
+    "Hear about the new restaurant called Karma? There’s no menu: You get what you deserve."
+]
+
+fun_phrase = random.choice(jokes)
+print(fun_phrase)
